72ce2fc28ad8ca9efe32b0e645e83e0d3f8d2e60
suhani chauhan <suhani.chauhan@campus.dsvv.in>
2025-07-08 16:39:03 +0530

    Change Image Slider Size

--- src/components/GallerySlider.jsx
+++ src/components/GallerySlider.jsx
@@ -19,7 +19,6 @@ const GallerySlider = ({ images }) => {
   return (
     <div id="gallery" className="py-5 bg-light">
       <div className="container">
-        <h2 className="text-center mb-4 text-primary">Visit Our Gallery</h2>
         <div className="slider mt-4">
           <div className="d-flex align-items-center gap-4 p-4 slider-track">
             {images.map((img, idx) => (
