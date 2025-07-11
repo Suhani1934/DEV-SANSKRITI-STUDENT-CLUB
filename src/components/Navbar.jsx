ceb1b2737e4533965f22e696a1dd6f00eb211c57
suhani chauhan <suhani.chauhan@campus.dsvv.in>
2025-07-11 10:53:13 +0530

    Add clubs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -67,7 +67,7 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <Link to="/clubs" className="nav-link nav-animated-link">
-                <i ></i>Clubs
+                <i className="bi bi-trophy-fill me-1" ></i>Clubs
               </Link>
             </li>
             <li className="nav-item">
